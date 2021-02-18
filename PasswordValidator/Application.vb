52a96f5c3bc9a4c626a1b0f965631326b342b2bb
torranca <torranca@icloud.com>
2021-02-18 22:46:28 +0000

    1st in 10 years

---
+++ PasswordValidator/Application.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Console
+
+Public Class Application
+	Public Shared Sub Main()
+		WriteLine("Hello, what is your name?")
+		Dim input as String = ReadLine()
+		WriteLine("Hello " +  input + "!")	
+	End Sub
+End Class
